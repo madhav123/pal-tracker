04d50ccf9546d9372e49dd1fcd7f42b0d76ead9a
training <madhav123>
2020-01-28 10:31:15 -0800

    lab1

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+public class PalTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+
+
+}
+
